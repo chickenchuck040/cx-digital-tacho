6888a357b45e66c986d934fe97f8c31d5d29402c
Tim <haytowntim@embarqmail.com>
2017-05-03 20:03:49 -0400

    Works

--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,125 @@
-//----------------------------------------------------------------------
-// https://github.com/clearwater/SwitecX25
-// 
-// This is an example of using the SwitchX25 library.
-// It zero's the motor, sets the position to mid-range
-// and waits for serial input to indicate new motor positions.
-// 
-// Open the serial monitor and try entering values 
-// between 0 and 944.
-// 
-// Note that the maximum speed of the motor will be determined
-// by how frequently you call update().  If you put a big slow
-// serial.println() call in the loop below, the motor will move
-// very slowly!
-//----------------------------------------------------------------------
+//
+// CX Digital Tachometer
+// Tim Hollabaugh
+//
 
 #include <Arduino.h>
 #include <SwitecX25.h>
 
+#define DEBUG
+
+#define LED 13
+#define INTERRUPT_PIN 2
+
 // standard X25.168 range 315 degrees at 1/3 degree steps
 #define STEPS (315*3)
 
-#define INTERRUPT_PIN 2
+#define TIMEOUT 1000
 
 #define GEAR_RATIO 24/43
-
 #define STEPS_PER_DEGREE 3
-
 #define DEGREE_PER_RPM 30.0/1000.0
+
+// The number of pulses to average
+#define DELTAS 20
 
 // For motors connected to digital pins 4,5,6,7
 SwitecX25 motor1(STEPS,5,6,7,8);
+
+// Buffer for the rolling average of input pulses
+unsigned long deltas[DELTAS];
 
 unsigned long lastIntTime = 0;
 unsigned long intTime = 0;
 
 void interrupt(){
-  lastIntTime = intTime;
-  intTime = micros();
+  
+  // Only get the pulse length on the falling edge
+  // The falling edge is steeper and more accurate
+  
+  if(digitalRead(INTERRUPT_PIN)){
+    digitalWrite(LED, HIGH);
+  }else{
+    lastIntTime = intTime;
+    intTime = micros();
+    digitalWrite(LED, LOW);
+  }
 }
 
-void setup(void)
-{
+void setup(void){
   // run the motor against the stops
   motor1.zero();
-  // start moving towards the center of the range
-  //motor1.setPosition(STEPS/2);
   
-  Serial.begin(9600);
+#ifdef DEBUG
+  Serial.begin(115200);
   Serial.println("STARTING");
+#endif // DEBUG
 
-  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), interrupt, RISING); 
+  pinMode(LED, OUTPUT);
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), interrupt, CHANGE); 
 }
 
-void loop(void)
-{
-  //static int nextPos = 0;
-  // the motor only moves when you call update
+void loop(void){
+
   motor1.update();
 
   unsigned long now = millis();
 
   static unsigned long last_calc = now;
 
-  if(now - last_calc > 100){
-    long delta = intTime - lastIntTime; 
-    Serial.print(delta);
-    Serial.print(":");
+  // Do the rpm calculations and update the motor every 50ms
+  if(now - last_calc > 50){
 
-    double deltasec = delta/1000000.0; 
-    Serial.print(deltasec);
-    Serial.print(":");
+    // Check if there was a pulse within the timeout
+    if(now - intTime < TIMEOUT){
+      long delta = intTime - lastIntTime; 
 
-    double pps = 1.0/deltasec;
-    Serial.print(pps);
-    Serial.print(":");
-    
-    double ppm = pps*60.0;
-    Serial.print(ppm);
-    Serial.print(":");
-    
-    double rpm = ppm/2.0;
-    Serial.print(rpm);
-    Serial.print(":");
+      // Create a rolling average of the deltas
+      unsigned long sum = 0;
+      unsigned long count = 0;
 
-    double steps = rpm * DEGREE_PER_RPM * STEPS_PER_DEGREE * GEAR_RATIO;
-    Serial.print(steps);
-    Serial.print(":");
+      // Move everything down one spot
+      for(int i = 0; i < DELTAS - 1; i++){
+        deltas[i] = deltas[i+1];
+        if(deltas[i] > 0){
+          sum += deltas[i];
+          count++;
+        }
+      }
 
-    Serial.println();
+      // Add the most recent delta to the end
+      if(delta != 0){
+        deltas[DELTAS - 1] = delta;
+        sum += delta;
+        count++;
+      }
 
-    motor1.setPosition(steps);
+      unsigned long average_delta = sum/count;
+      double deltasec = average_delta/1000000.0; 
+      double pps = 1.0/deltasec;
+      double ppm = pps*60.0;
+      double rpm = ppm/2.0;
+      double steps = rpm * DEGREE_PER_RPM * STEPS_PER_DEGREE * GEAR_RATIO;
+
+#ifdef DEBUG
+      Serial.print(delta);
+      Serial.print(":");
+      Serial.print(average_delta);
+      Serial.print(":");
+      Serial.print(rpm);
+      Serial.print(":");
+      Serial.print(steps);
+      Serial.println();
+#endif // DEBUG
+
+      motor1.setPosition(steps);
+    }else{
+      motor1.setPosition(0);
+    }
 
     last_calc = now;
   }
-
-  static unsigned long last_update = now;
-  static bool last_pos = false;
-
-  if(now - last_update > 1000){
-    if(last_pos){
-      //motor1.setPosition(0);
-    }else{
-      //motor1.setPosition(STEPS-1);
-    }
-    last_pos = !last_pos;
-    last_update = now;
-  }
-  
-  /*
-  if (Serial.available()) {
-    char c = Serial.read();
-    if (c==10 || c==13) {
-      motor1.setPosition(nextPos);
-      nextPos = 0;
-    } else if (c>='0' && c<='9') {
-      nextPos = 10*nextPos + (c-'0');
-    }
-  }
-  */
 }
 
